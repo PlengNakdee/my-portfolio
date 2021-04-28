ff87ce5454591e29a2f83778c9a5aa09704a0422
Pleng Nakdee <plengwn@gmail.com>
2021-04-28 11:55:46 +0700

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio-Pleng-Nakdee</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>hello world</h1>
+    </div>
+  );
+}
