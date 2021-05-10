95a85a9a2d6eb5912e1d6fb07ce7d7e82218e093
Pleng Nakdee <plengwn@gmail.com>
2021-05-11 00:38:31 +0700

    add about me

--- pages/contact.js
+++ pages/contact.js
@@ -9,6 +9,28 @@ function contact() {
         <div className="text-6xl mb-6">
           <p>Contact</p>
         </div>
+        <div className="text-lg">
+          <p className="mt-4">I'd love to hear from you.</p>
+          <p>
+            You can email me at{" "}
+            <a href="mailto:plengwn@gmail.com" target="_blank">
+              plengwn@gmail.com
+            </a>{" "}
+          </p>
+          <p>Or connect through my</p>
+          <p>
+            <a href="https://www.linkedin.com/in/plengnakdee/" target="_blank">
+              LinkedIn
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://github.com/PlengNakdee?tab=repositories"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </p>
+        </div>
       </div>
       <Footer />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,18 @@ export default function Home() {
       <div className="ml-4 mt-14 sm:ml-10 lg:ml-20 flex-grow">
         <div>
           <p className="text-9xl">Hi,</p>
-          <p className="text-4xl mt-4">
-            I'm a fullstack developer / Cloud Engineer
+          <p className="text-4xl mt-4 mr-6">
+            I'm a Fullstack Developer / Cloud Engineer
           </p>
           <p></p>
         </div>
-        <div className="text-lg mt-4">
-          <p>About me</p>
+        <div className="text-lg mt-4 mr-6">
+          <p>
+            I enjoy the analytical and problem-solving aspects that come with
+            coding. My current work involves native cloud web applications,
+            which focuses on front-end. I’m also an Ai enthusiast with some
+            experience in deep learning.{" "}
+          </p>
         </div>
       </div>
       <Footer />
