15537bd7bee05990471ba80031c8c354593a0a6f
PlengNakdee <plengwn@gmail.com>
2021-10-01 08:40:34 +0000

    modify projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,52 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, web } = require('projen');
+
+const projectName = 'my-portfolio';
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
+  cdkVersionPinning: true,
   defaultReleaseBranch: 'main',
-  name: 'my-portfolio',
+  name: `${projectName}-backend`,
 
-  // cdkDependencies: undefined,  /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                    /* Runtime dependencies of this module. */
-  // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                 /* Build dependencies for this module. */
-  // packageName: undefined,      /* The "name" in package.json. */
-  // release: undefined,          /* Add release management to this project. */
+  cdkDependencies: [
+    '@aws-cdk/aws-s3-deployment',
+    '@aws-cdk/aws-s3',
+  ],
+
 });
 project.synth();
+
+const frontend = new web.ReactTypeScriptProject({
+  defaultReleaseBranch: 'main',
+  outdir: 'frontend',
+  parent: project,
+  name: `${projectName}-frontend`,
+
+      /* spellchecker: disable */
+      deps: [
+        "tailwindcss@^2",
+        "@tailwindcss/typography",
+        "@tailwindcss/forms",
+        "@tailwindcss/aspect-ratio",
+        "@tailwindcss/line-clamp",
+        "postcss@^8",
+        "autoprefixer@^9",
+        "@heroicons/react",
+        "@headlessui/react",
+        "react",
+        "react-dom",
+        "react-scroll",
+      ],
+  
+      devDeps: [
+        "@types/react-scroll",
+      ],
+      /* spellchecker: enable */
+
+});
+
+// frontend.addTask('dev', {
+//   description: 'Runs the application locally',
+//   exec: 'react-scripts start',
+// });
+
+frontend.synth();
